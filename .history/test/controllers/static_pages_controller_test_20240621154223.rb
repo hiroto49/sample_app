979e599e26c6a914e775936be52374bd66ffc6db
hiroto49 <s214230@kansaigaidai.jp>
2024-06-21 15:44:30 +0900

    Complete advanced testing setup

--- .history/test/controllers/static_pages_controller_test_20240621154223.rb
+++ .history/test/controllers/static_pages_controller_test_20240621154223.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get static_pages_home_url
     assert_response :success
-    
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get home" do
